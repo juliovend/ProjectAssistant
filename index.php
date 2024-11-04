b1aa70e4befc63f2b4992175bee94919ee8daa20
juliovend <juliovend@yahoo.fr>
2024-11-05 00:46:59 +0100

    accueil

--- index.php
+++ index.php
@@ -279,7 +279,7 @@ header("Content-Type: text/html; charset=UTF-8");
         <li><strong>Analyses intelligentes :</strong> Suivez votre avancement avec des analyses automatiques.</li>
         <li><strong>Accessible partout :</strong> Gérez vos projets depuis n’importe quel appareil, à tout moment.</li>
     </ul>
-    <img src="/data/PA_Snapshot.JPG" alt="Aperçu de Project Assistant" style="max-width: 100%; height: auto; border-radius: 10px;">
+    <img src="/data/PA_Snapshot.JPG" alt="Aperçu de Project Assistant" style="max-width: 100%; height: auto; border-radius: 10px;top-margin: 20 px;">
 </div>
 
 
