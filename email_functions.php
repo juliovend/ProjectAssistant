1597086288e22910b697ff5c77ec391b66f87da9
juliovend <juliovend@yahoo.fr>
2024-11-05 00:16:49 +0100

    accueil

--- email_functions.php
+++ email_functions.php
@@ -4,10 +4,10 @@ function sendVerificationEmail($email, $fullName, $verification_token) {
     $subject = "Validez votre inscription";
     $message = "Bonjour $fullName,\n\n";
     $message .= "Merci de vous être inscrit sur notre plateforme. Cliquez sur le lien suivant pour valider votre inscription :\n\n";
-    $message .= "https://darkgreen-emu-352810.hostingersite.com/verify.php?token=$verification_token\n\n";
+    $message .= "https://www.project-assistant.tech/verify.php?token=$verification_token\n\n";
     $message .= "Si vous n'avez pas créé de compte, veuillez ignorer cet email.";
 
-    $headers = "From: no-reply@project-assistant.com\r\n";
+    $headers = "From: no-reply@project-assistant.tech\r\n";
     return mail($to, $subject, $message, $headers);
 }
 ?>
